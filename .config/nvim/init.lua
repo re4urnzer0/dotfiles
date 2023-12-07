dbf54df9b1ac3bb2b4c839ded3cf0d78687449c5
re4urnzer0 <maverickjin@outlook.com>
2023-12-07 17:31:48 +0800

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,8 +5,7 @@ if vim.g.vscode then
     vim.opt.hlsearch = false
 else
     -- Bases
-    require('base/options')
-    require('base/keymaps')
+    require('base')
     --Plugins
     -- lazy.nvim
     require('plugins')
@@ -14,36 +13,5 @@ else
     require('cs')
 
     -- Plugins
-    -- autoclose.nvim
-    require('module.ac')
-    -- indent-blankline.nvim
-    require('module.indent')
-    -- neo-tree.nvim
-    require('module.sidebar')
-    -- alpha.nvim
-    require('module.startup')
-    -- barbar.nvim
-    require('module.tabline')
-    -- telescope.nvim
-    require('module.ts')
-    -- LSP
-    require('module.lsp')
-    -- nvim-cmp
-    require('module.nvim-cmp')
-    -- neoscroll.nvim
-    require('module.scroll')
-    -- cursorline.nvim
-    require('module.cursorline')
-    -- treesitter.nvim
-    require('module.treesitter')
-    -- lualine.nvim
-    require('module.statusline')
-    -- noice.nvim
-    require('module.notice')
-    -- trouble.nvim
-    require('module.diag')
-    -- symbol-outline.nvim
-    require('module.outline')
-    -- gitsigns.nvim
-    require('module.git')
+    require('module')
 end
